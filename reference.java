e346d98992c2c0b8368571333919dcc4e7b596b0
Dai Yamamoto <daiaiaiai.123xyz@gmail.com>
2016-09-09 14:44:42 +0900

    Update reference.java

--- reference.java
+++ reference.java
@@ -2,7 +2,8 @@
 @import controllers.admin.form.AdminSearchForm
 
 
-// 　actionのところではclass がいらない
+// 　actionのところではclass がいらない0
+//form  は上の変数のフォームの意味変数
   @helper.form(action = controllers.admin.routes.AdminAbcController.showList()) {
     @helper.inputText(form(AdminSearchForm.KEY_NAME), 'class -> "aaaa");
     
